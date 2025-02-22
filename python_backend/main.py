7e587adfdb292aa4c8e467f31f7279cb8900fd70
justynaprzyborska <justyna.przyborska123@gmail.com>
2025-02-22 21:51:58 +0000

    frontend

--- python_backend/main.py
+++ python_backend/main.py
@@ -29,6 +29,9 @@ s3_client = boto3.client('s3',
 def home():
     return render_template("/index.html")
 
+@app.route("/notes")
+def notes():
+    return render_template("/notes.html")
 
 
 @app.route("/uploadImageQuery", methods=["POST"])
